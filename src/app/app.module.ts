60a7ce81c8e3924f78d51a5f2753597dca4efaab
Darren152 <tdarren27@gmail.com>
2020-11-06 01:15:49 -0500

    fuck this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
-  {path: "products", component: "ProductListComponent"},
-  {path: "product/:id", component: "ProductPageComponent"},
-  {path: "", redirectTo: "products", pathMatch: "full"}
+  { path: 'products', component: 'ProductListComponent'},
+  { path: 'product/:id', component: 'ProductPageComponent'},
+  { path: '', redirectTo: 'products', pathMatch: 'full'}
   ];
 
 @NgModule({
@@ -28,8 +28,9 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
